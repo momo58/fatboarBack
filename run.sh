deaf425d9eb70491f801bf51c70ed79fb44de0c0
Khamassi_Amira <amira.khamassi@gmail.com>
2019-10-30 15:17:58 +0100

    trying to install docker-compose via the run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-ls -la /usr/local/bin/
+curl -L https://github/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+ls -la /usr/local/bin
