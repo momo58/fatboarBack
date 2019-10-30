c6dd0c9f220931b2bd166f25264c8d6e69cc72bd
Khamassi_Amira <amira.khamassi@gmail.com>
2019-10-30 15:20:30 +0100

    After successfully installing docker-compose, i'm calling docker-compose -up

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 curl -L https://github/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 ls -la /usr/local/bin
+docker-compose -up
