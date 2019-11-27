9d4081454717654ffb2d0a3c0d7916ce048600c0
Khamassi_Amira <amira.khamassi@gmail.com>
2019-11-27 12:43:50 +0100

    Creating a test profile a test database

--- src/test/java/com/pfa/fatboar/FatboarBack/controllers/TicketControllerTest.java
+++ src/test/java/com/pfa/fatboar/FatboarBack/controllers/TicketControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class TicketControllerTest {
 
     @Autowired
