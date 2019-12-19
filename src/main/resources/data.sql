bd052d85301c1d408e16eff874a2d12b2482bf2b
valentinlourteau <valentinlourteau@gmail.com>
2019-12-19 09:07:11 +0100

    correctifs datas.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,16 @@ INSERT IGNORE INTO gains (id, label) VALUES
 (6, 'Range Rover Evoque');
 
 --Insertion de l'administrateur du jeu concours
-INSERT IGNORE INTO utilisateurs values (1, 'superadmin', null, '$2a$10$nUPHp/wkeKy1P20lnF3y4uFFlh8soXSkk2tqK9cnnPX4MkggsQAsG', 0, null, 'superadmin', 'superadmin');
+INSERT IGNORE INTO utilisateurs
+(id, username, email, password, sub, image_url, role, clear_pwd)
+values (1, 
+'superadmin', 
+'superadmin', 
+'$2a$10$nUPHp/wkeKy1P20lnF3y4uFFlh8soXSkk2tqK9cnnPX4MkggsQAsG', 
+null,
+null,  
+0, 
+'superadmin');
 
 --Insertion de la présentation du jeu concours
 INSERT IGNORE INTO game (id, content) values (1, 'Jeu concours fatboar à administrer');
